5ffdf88154555210de74e7c5d83b8dc1c74e647f
mariotbg11 <christofell.m@gmail.com>
2023-04-15 18:10:00 +0700

    Commit file

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      boxShadow: {
+        "bx-sd": "5px 5px 0 0 #b91c1c",
+      },
+      colors: {
+        primary: "#1c262f",
+        primarylight: "#212d37",
+        textlight: "#f8f9fa",
+      },
+      borderRadius: {
+        circle: "50%",
+      },
+      width: {
+        500: "31.25rem",
+      },
+      height: {
+        500: "31.25rem",
+      },
+      maxWidth: {
+        "8xl": "1310px",
+      },
+      screens: {
+        xsm: "400px",
+        // => @media (min-width: 400px) { ... }
+      },
+    },
+  },
+  plugins: [],
+};
